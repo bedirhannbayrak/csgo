cb199271dba09afb8bdb89b533c7873490f40dd0
bedirhannbayrak <bedirhan.bayrak41@gmail.com>
2021-01-25 22:12:28 +0300

    API,json-server dynamic data

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,15 +12,17 @@ class SearchBar extends React.Component {
     render() {
         return (
             <form onSubmit={this.handleFormSubmit}>
-                <div className='form-row mb-5 mt-2'>
-                    <div className='col-12'>
-                        <input onChange={(event)=> this.setState({searchQuery: event.target.value})} 
-                        type='text' className='form-control' 
-                        placeholder='Search Movie'
-                        value = {this.state.searchQuery}></input>
-                    </div>
-                    </div>        
-            </form>
+            <div className="form-row mb-5">
+                <div className="col-12">
+                    <input 
+                    
+                        onChange={this.props.searchMovieProp} 
+                        type="text" className="form-control" 
+                        placeholder="Seach a movie" 
+                    />
+                </div>
+            </div>
+        </form>
         )
     }
 }
