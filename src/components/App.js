4e19935f2d0711f51e38f367678d7bacea9315e1
bedirhannbayrak <bedirhan.bayrak41@gmail.com>
2021-01-26 21:02:38 +0300

    local api'ye geçildi, Router,react.fragment kullanıldı

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+
 
 
 class App extends React.Component {
-
-
-
 
     state = {
         movies: [],
@@ -15,13 +15,6 @@ class App extends React.Component {
         searchQuery: ""
     }
 
-    // async componentDidMount(){
-    //   const baseURL = "http://localhost:3003/movies";
-    //   const response = await fetch(baseURL);
-    //   const data = await response.json();
-    //   this.setState({movies:data})
-    // }
-    
     async componentDidMount() {
         const response = await axios.get('http://localhost:3003/movies');
         console.log(response)
@@ -29,30 +22,12 @@ class App extends React.Component {
 
     }
 
-    
-/*  FETCH API DELETE METHODU
-
-    deleteMovie = async (movie) => {
-        const baseURL = `http://localhost:3003/movies/${movie.id}`;
-        await fetch(baseURL, {
-            method: 'DELETE'
-        })
-
-        const newMovieList = this.state.movies.filter(
-            m => m.id !== movie.id
-        );
-        */
-
        deleteMovie = async (movie) => {
         const baseURL = `http://localhost:3003/movies/${movie.id}`;
         await axios.delete(baseURL);
         const newMovieList = this.state.movies.filter(
             m => m.id !== movie.id
         );
-
-/*         this.setState ({
-            movies: newMovieList
-        }) */
 
         this.setState(state => ({
             movies: newMovieList
@@ -73,17 +48,28 @@ class App extends React.Component {
         )
 
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-12">
-                        <SearchBar searchMovieProp={this.searchMovie} />
-                    </div>
-                </div>
+            <Router>
+                <div className="container">
+                
+                    <Route path='/' exact render={() => (
+                        <React.Fragment>
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <SearchBar searchMovieProp={this.searchMovie} />
+                                </div>
+                            </div>
 
-                <MovieList
-                    movies={filteredMovies}
-                    deleteMovieProp={this.deleteMovie} />
-            </div>
+                        <MovieList
+                            movies={filteredMovies}
+                            deleteMovieProp={this.deleteMovie} />
+                        </React.Fragment>
+                     )}>
+
+                    </Route>
+                    <Route path='/add' component={AddMovie} />
+                   
+                </div>
+            </Router>
         )
 
     }
