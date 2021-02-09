65dcb52476a811f0ed852c097d746fa03f8de4a5
bedirhannbayrak <bedirhan.bayrak41@gmail.com>
2021-02-09 18:23:53 +0300

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { useTodoLayerValue } from '../context/TodoContext'
-import TodoList from './TodoList'
-import './App.css'
+import React from 'react'
 
 const App = () => {
-    
-    const [ { todos } , dispatch ] = useTodoLayerValue();
-    const [content, setContent] = useState("")
-    const inputRef=useRef(null);
-
-    useEffect(()=> {
-        inputRef.current.focus();
-    })
-
-
-    const handleSubmit = (e) => {
-        console.log(content)
-        e.preventDefault();
-        
-        if (!content && content.length < 1 ) return;
-        const newTodo = {
-            id: Math.floor(Math.random() * 654656),
-            content,
-            isCompleted:false
-        };
-
-        dispatch({
-            type: 'ADD_TODO',
-            payload: newTodo
-        });
-
-        setContent('');
-    }
     return (
-        <div className="container">
-            <form onSubmit={handleSubmit} className="todo-form" >
-                <input type="text" className="todo-input" onChange={event=>setContent(event.target.value)} value={content} ref={inputRef} />
-                <button className="todo-button">
-                    ekle
-                </button>
-            </form>
-            
-            <TodoList todos={todos} />
+        <div>
+            <h1>hello</h1>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 
 import App from './components/App';
-import { TodoLayer } from './context/TodoContext';
-import reducer, { initialState } from './context/reducer'
 
 ReactDOM.render(
     <React.StrictMode>
-        <TodoLayer initialState={initialState} reducer={reducer}>
             <App />
-        </TodoLayer>
     </React.StrictMode>
 , 
 document.getElementById('root')
